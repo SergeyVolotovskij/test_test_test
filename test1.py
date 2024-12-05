588dbd24921097c813e92a7b0d758da468022836
Sergey Volotovskij <sergeyvolotovskij@gmail.com>
2024-12-05 17:45:51 +0300

    Added new functions

---
+++ test1.py
@@ -0,0 +1,20 @@
+# test1.py
+def func(*args, **kwargs):
+    print(f"args: {args}")
+    print(f"kwargs: {kwargs}")
+
+a: int = 10
+b: int = 101.3
+c: int = 200
+
+func(a, b, c, p=4, z=6, x=50, v='HelloWord')
+
+# args: (10, 101.3, 200)
+# kwargs: {'p': 4, 'z': 6, 'x': 50, 'v': 'HelloWord'}
+
+from typing import Union
+def func2(*args, a: Union[int] = None, b: Union[int] = None):
+    print(f"args: {args}")
+    print(f"a / b: {a} / {b}")
+
+func2(1,2,3,4,5,6,7, a=500, b=600)
